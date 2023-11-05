adcd0ccf2d40c373951d93089def92b458d9f5a1
alastovina <alastovina@i-neti.ru>
2023-11-05 21:59:04 +0300

    эндпоинты вынесены в отдельную папку

---
+++ api_v1/routes/v1_router.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+from keras.models import load_model
+from PIL import Image
+from fastapi import UploadFile, File, Depends, APIRouter
+
+import tensorflow as tf
+from typing import Dict
+
+v1_router = APIRouter()
+
+def model_processing(file):
+    """
+    Predicting probability of AP-ROP
+    """
+    model_cnn = load_model('modelCNN.h5', compile=False)
+    model_cnn.compile()
+    image = Image.open(file)
+    img_array = np.array(image)
+    prediction = model_cnn.predict(
+        tf.reshape(img_array, [1, 480, 640, 3]))
+    probability: float = round(float(prediction[0][0]), 5)
+    if probability > 0.7:
+        text = f'\n{probability * 100}% вероятность ЗАРН\nВысокая вероятность ЗАРН, требуется проверка!\n________________________________________'
+    else:
+        text = f'\n{probability * 100}% вероятность ЗАРН\n________________________________________'
+    return text
+
+@v1_router.post("/upload")
+def upload(file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        res_folder = os.path.join('res', file.filename)
+        with open(res_folder, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+    try:
+        result = model_processing(res_folder)
+        return {"message": result}
+    except Exception:
+        return {"message": "There was an error processing the file"}
+
--- main.py
+++ main.py
@@ -1,45 +1,25 @@
-import os
-import numpy as np
-from keras.models import load_model
-from PIL import Image
-from fastapi import FastAPI, UploadFile, File
-import tensorflow as tf
+import logging
+
+from fastapi import FastAPI
+
+from api_v1.routes.v1_router import v1_router
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+logger.info('Adding v1 endpoints...')
 
-def model_processing(file):
-    """
-    Predicting probability of AP-ROP
-    """
-    model_cnn = load_model('modelCNN.h5', compile=False)
-    model_cnn.compile()
-    image = Image.open(file)
-    img_array = np.array(image)
-    prediction = model_cnn.predict(
-        tf.reshape(img_array, [1, 480, 640, 3]))
-    probability: float = round(float(prediction[0][0]), 5)
-    if probability > 0.7:
-        text = f'\n{probability * 100}% вероятность ЗАРН\nВысокая вероятность ЗАРН, требуется проверка!\n________________________________________'
-    else:
-        text = f'\n{probability * 100}% вероятность ЗАРН\n________________________________________'
-    return text
+app.include_router(v1_router, prefix='/v1')
 
-@app.post("/upload")
-def upload(file: UploadFile = File(...)):
-    messages = {}
-    try:
-        contents = file.file.read()
-        res_folder = os.path.join('res', file.filename)
-        with open(res_folder, 'wb') as f:
-            f.write(contents)
-    except Exception:
-        return {"message": "There was an error uploading the file"}
-    finally:
-        file.file.close()
-    try:
-        result = model_processing(res_folder)
-        return {"message": result}
-    except Exception:
-        return {"message": "There was an error processing the file"}
+@app.on_event('startup')
+async def startup():
+    ...
+
+@app.on_event('shutdown')
+async def shutdown():
+    ...
+
+
+
 
